34beaeb2d89e2b3f400db0b82fcff9f16106f369
Thao <20110197@student.hcmute.edu.vn>
2023-05-10 19:33:15 +0700

    get /

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to API Book App');
+});
+
 initRoutes(app);
 
 const PORT = process.env.PORT || 8888;
